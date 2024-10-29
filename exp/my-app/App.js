5c53a608499e5397c4b4e632c9b157a45971f01d
Vijay_Reddy <148469638+Rengoku-demon@users.noreply.github.com>
2024-10-29 19:40:17 +0530

    Add files via upload

---
+++ exp/my-app/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PasswordStrengthChecker from './components1/PasswordStrengthChecker';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <PasswordStrengthChecker />
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
