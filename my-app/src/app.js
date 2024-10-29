2865a8964303178cf68b9fb142600c6453fad29d
Vijay_Reddy <148469638+Rengoku-demon@users.noreply.github.com>
2024-10-29 19:45:10 +0530

    Create app.js

---
+++ my-app/src/app.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PasswordStrengthChecker from './components1/PasswordStrengthChecker';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <PasswordStrengthChecker />
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+//apptest.js
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
